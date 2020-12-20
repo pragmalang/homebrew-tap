91b81abb16c5f7da37482d791ddf3a104667360a
Tabzz98 <muhammadtabaza@gmail.com>
2020-12-20 06:21:10 +0200

    Pragma 0.2.0 release

--- pragma.rb
+++ pragma.rb
@@ -1,9 +1,9 @@
 class Pragma < Formula
     desc "The Pragma CLI"
     homepage "https://github.com/pragmalang/pragma"
-    url "https://github.com/pragmalang/pragma/releases/download/v0.1.0/pragma-universal.zip"
-    sha256 "fbac9131942c17ac80f9c3dea27332890496dfcb0135e4f3c4018b2de237bdd1"
-    version "0.1.0"
+    url "https://github.com/pragmalang/pragma/releases/download/0.2.0/pragma-0.2.0-universal.zip"
+    sha256 "e2b2d2e2061be15b1bbd50f61637d92ecef1e3ace6ddbd325a026ad5fd818354"
+    version "0.2.0"
     license "Apache-2.0"
   
     bottle :unneeded
