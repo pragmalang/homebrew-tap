0e8c361937ea2d1a8fa8e4b63183357b93dbb7a1
Tabzz98 <muhammadtabaza@gmail.com>
2020-12-23 19:54:35 +0200

    .

--- pragma.rb
+++ pragma.rb
@@ -8,6 +8,9 @@ class Pragma < Formula
   
     bottle :unneeded
 
+    depends_on java: "1.11+" if MacOS.version < :catalina
+    depends_on "openjdk" if MacOS.version >= :catalina
+
     def install
       prefix.install "lib"
       begin
