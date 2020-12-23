25b23c2ea314c15c8d0345640d2447351dd058cb
Tabzz98 <muhammadtabaza@gmail.com>
2020-12-23 23:08:25 +0200

    .

--- pragma.rb
+++ pragma.rb
@@ -1,32 +1,17 @@
 class Pragma < Formula
     desc "The Pragma CLI"
     homepage "https://github.com/pragmalang/pragma"
-    url "https://github.com/pragmalang/pragma/releases/download/0.2.0/pragma-0.2.0-macos.zip"
+    url "https://github.com/pragmalang/pragma/releases/download/0.2.0/pragma.jar"
     sha256 "aab25b35b03823fecb440fa2a64dddf4896d32dece810278aa7c42bcf51cb379"
     version "0.2.0"
     license "Apache-2.0"
   
     bottle :unneeded
 
-    depends_on java: "1.11+"
-
     def install
-      prefix.install "lib"
-      begin
-        prefix.install "jre"
-      rescue
-        opoo "JRE is not included with the installation. You may need to install Java."
-      end
-      prefix.install "bin"
-      prefix.install_symlink prefix/"bin"/"pragma"
-
-      Dir.glob(prefix/"*/*").each { |file| 
-        begin
-          system "xattr", "-d", "com.apple.quarantine", file
-          ohai "Removed com.apple.quarantine xattr attribute from #{file}"
-        rescue
-          ohai "No com.apple.quarantine xattr attribute on #{file}"
-        end
-      }
+      bin.install "pragma.jar"
+      File.write(bin/"pragma", "#!/bin/bash\njava -jar pragma.jar \"$@\" || echo 'Could not run java command. Please install Java from https://www.oracle.com/java/technologies/javase-jdk15-downloads.html before running pragma.' && false")
+      system "chmod" "+x" bin/"pragma"
+      prefix.install_symlink bin/"pragma"
     end
 end
